fb5539891480d07ac7f64d03d5e964ba65fed22c
Jason Brown <jasontbrown99@gmail.com>
2018-01-24 00:33:34 -0800

    create image link submission form with basic css

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <Navigation />
-      <Logo />
-    {/*<Navigation />
-    <Fa />*/}
+        <Navigation />
+        <Logo />
+        <ImageLinkForm />
 
       </div>
     );
