9cb947e7c37735633a4bbb901f75ee364486ab2f
Jason Brown <jasontbrown99@gmail.com>
2018-01-23 23:43:59 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Navigation from './components/Navigation/Navigation';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <Navigation />
+    {/*<Logo />
+    <Navigation />
+    <Fa />*/}
+
+      </div>
+    );
+  }
+}
+
+export default App;
