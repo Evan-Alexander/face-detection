3416425742d3b39c50c3fb3a5f0494ae4a0077c0
Jason Brown <jasontbrown99@gmail.com>
2018-01-25 16:07:15 -0800

    allow users to register to the app

--- src/App.js
+++ src/App.js
@@ -34,14 +34,25 @@ class App extends Component {
       imageUrl: '',
       box: {},
       route: 'signin',
-      isSignedIn: false
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
     }
   }
 
-  componentDidMount() {
-    fetch('http://localhost:3000')
-      .then(response => response.json())
-      .then(console.log)
+  loadUser = (data) => {
+    this.setState({ user: {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
+    }})
   }
 
   calculateFaceLocation = (data) => {
@@ -111,7 +122,9 @@ class App extends Component {
             : (
               route === 'signin'
               ? <SignIn onRouteChange={this.onRouteChange}/>
-              : <Register onRouteChange={this.onRouteChange}/>
+              : <Register
+                loadUser={this.loadUser}
+                onRouteChange={this.onRouteChange}/>
             )
         }
       </div>
